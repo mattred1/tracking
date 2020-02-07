ad3ac06c5c6de785576fb8a4754a348a2275bfc4
Matt Redinger <mattred1@gmail.com>
2020-02-06 22:07:06 -0500

    Fix sticky menu

--- scripts.js
+++ scripts.js
@@ -1,17 +1,14 @@
-// When the user scrolls the page, execute myFunction
-window.onscroll = function () { myFunction() };
 
-// Get the navbar
-var navbar = document.getElementById("navbar");
 
-// Get the offset position of the navbar
-var sticky = navbar.offsetTop;
+var div_top = $('.menu').offset().top;
 
-// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function myFunction() {
-    if (window.pageYOffset >= sticky) {
-        navbar.classList.add("sticky")
+$(window).scroll(function () {
+    var window_top = $(window).scrollTop() - 0;
+    if (window_top > div_top) {
+        if (!$('.menu').is('.sticky')) {
+            $('.menu').addClass('sticky');
+        }
     } else {
-        navbar.classList.remove("sticky");
+        $('.menu').removeClass('sticky');
     }
-} 
+});
